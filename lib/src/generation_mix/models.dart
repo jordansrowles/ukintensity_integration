fefb31c0a225942979ccac1caec8e60ed7447b5d
jordansrowles <jordansrowles@gmail.com>
2022-11-06 19:11:00 +0000

    Added GenerationMixList

--- lib/src/generation_mix/models.dart
+++ lib/src/generation_mix/models.dart
@@ -19,6 +19,29 @@ class GenerationMix {
   }
 }
 
+class GenerationMixList {
+  List<GenerationMixData>? data;
+
+  GenerationMixList({this.data});
+
+  GenerationMixList.fromJson(Map<String, dynamic> json) {
+    if (json['data'] != null) {
+      data = <GenerationMixData>[];
+      json['data'].forEach((v) {
+        data!.add(new GenerationMixData.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
 class GenerationMixData {
   String? from;
   String? to;
